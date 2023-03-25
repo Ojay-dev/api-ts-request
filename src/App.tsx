1891fa5066a0947b447d5d54e807bf2edfc451b3
Ojonugwa Alikali <alikali.ojonugwajustice@gmail.com>
2023-03-25 18:14:29 +0100

    Update hooks nand App usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useApiRequest } from './hooks/userApiRequest';
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com/todos';
 
 type Todo = {
 	userId: number;
@@ -9,11 +11,42 @@ type Todo = {
 };
 
 function App() {
-	const { error, isLoaded, data, get } = useApiRequest<Todo[]>();
+	const { error, isLoaded, data, get, post, put, del } = useApiRequest<
+		Todo[] | Todo
+	>();
+
+	const [newTodoTitle, setNewTodoTitle] = useState<string>('');
 
 	useEffect(() => {
-		get('https://jsonplaceholder.typicode.com/todos');
+		get(BASE_URL);
 	}, [get]);
+
+	const handleNewTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setNewTodoTitle(event.target.value);
+	};
+
+	const handleNewTodoSubmit = async (
+		event: React.FormEvent<HTMLFormElement>
+	) => {
+		event.preventDefault();
+		const newTodo: Todo = {
+			userId: 1,
+			id: 201,
+			title: newTodoTitle,
+			completed: false
+		};
+		await post(BASE_URL, newTodo);
+		setNewTodoTitle('');
+	};
+
+	const handleTodoUpdate = async (todo: Todo) => {
+		const updatedTodo: Todo = { ...todo, completed: !todo.completed };
+		await put(`${BASE_URL}/${todo.id}`, updatedTodo);
+	};
+
+	const handleTodoDelete = async (todo: Todo) => {
+		await del(`${BASE_URL}/${todo.id}`);
+	};
 
 	if (error !== null) {
 		return <div>Error: {error.message}</div>;
@@ -22,14 +55,34 @@ function App() {
 	}
 
 	return (
-		<ol>
-			{(data as Todo[]).map((todo: Todo) => (
-				<li key={todo.id}>
-					<h2>{todo?.title}</h2>
-					<p>{todo?.completed ? 'Completed' : 'Not Completed'}</p>
-				</li>
-			))}
-		</ol>
+		<>
+			<h2>Add a New Todo</h2>
+			<form onSubmit={handleNewTodoSubmit}>
+				<label>
+					Title:
+					<input
+						type="text"
+						value={newTodoTitle}
+						onChange={handleNewTodoChange}
+					/>
+				</label>
+				<button type="submit">Add</button>
+			</form>
+
+			<h2>Todos</h2>
+			<ol>
+				{(data as Todo[]).map((todo: Todo) => (
+					<li key={todo.id}>
+						<h2>{todo?.title}</h2>
+						<p>{todo?.completed ? 'Completed' : 'Not Completed'}</p>
+						<button onClick={() => handleTodoUpdate(todo)}>
+							{todo?.completed ? 'Mark as Incomplete' : 'Mark as Complete'}
+						</button>
+						<button onClick={() => handleTodoDelete(todo)}>Delete</button>
+					</li>
+				))}
+			</ol>
+		</>
 	);
 }
 
--- src/hooks/userApiRequest.tsx
+++ src/hooks/userApiRequest.tsx
@@ -29,19 +29,26 @@ export function useApiRequest<T>() {
 	);
 
 	const post = useCallback(
-		(url: string, data?: T) =>
-			fetchData({ method: 'post', url, data: data || undefined }),
+		(url: string, data?: T) => {
+			setIsLoaded(false);
+			fetchData({ method: 'post', url, data: data || undefined });
+		},
 		[fetchData]
 	);
 
 	const put = useCallback(
-		(url: string, data?: T) =>
-			fetchData({ method: 'put', url, data: data || undefined }),
+		(url: string, data?: T) => {
+			setIsLoaded(false);
+			fetchData({ method: 'put', url, data: data || undefined });
+		},
 		[fetchData]
 	);
 
 	const del = useCallback(
-		(url: string) => fetchData({ method: 'delete', url }),
+		(url: string) => {
+			setIsLoaded(false);
+			fetchData({ method: 'delete', url });
+		},
 		[fetchData]
 	);
 
